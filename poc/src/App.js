f70fa79cdbd74db8f85540ae07a63daeab541d53
Soptq <32592090+soptq@users.noreply.github.com>
2022-02-25 16:14:31 +0800

    feat: refactor login system

--- poc/src/App.js
+++ poc/src/App.js
@@ -1,124 +1,128 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 
-import NodeWalletConnect from "@walletconnect/node";
-import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
-import Axios from 'axios';
-
-function convertIdToStr(chainId) {
-  switch (chainId) {
-    case 1:
-      return "eth";
-    case 97:
-      return "bsc";
-    case 137:
-      return "polygon";
-    default:
-      return "unknown";
-  }
-}
+const Web3 = require('web3');
 
 function App() {
-  const [bgClr, setBgClr] = React.useState("white");
+    const [web3, setWeb3] = React.useState(null)
+    const [connected, setConnected] = React.useState(false)
+    const [verified, setVerified] = React.useState(false)
+    const [haveToken, setHaveToken] = React.useState(false)
 
-  useEffect(() => {
-    async function fetchData() {
+    const tokenAddress = process.env.REACT_APP_TOKEN_ADDRESS
+    const tokenABI = JSON.parse(process.env.REACT_APP_TOKEN_ABI)
 
-      // Create connector
-      const walletConnector = new NodeWalletConnect(
-        {
-          bridge: "https://bridge.walletconnect.org",
-        },
-        {
-          clientMeta: {
-            description: "WalletConnect NodeJS Client",
-            url: "https://nodejs.org/en/",
-            icons: ["https://nodejs.org/static/images/logo.svg"],
-            name: "WalletConnect",
-          },
+    const verifyUser = async (address) => {
+        if (verified)
+            return true
+        // Requesting sign
+        const message = `I am the owner of the address ${address}`
+        const signedMessage = await web3.eth.personal.sign(web3.utils.utf8ToHex(message), address);
+        if (address === web3.eth.accounts.recover(message, signedMessage)) {
+            setVerified(true)
+            return true
         }
-      );
-      
-      // Check if connection is already established
-      if (!walletConnector.connected) {
-        // create new session
-        walletConnector.createSession().then(() => {
-          // get uri for QR Code modal
-          const uri = walletConnector.uri;
-          // display QR Code modal
-          WalletConnectQRCodeModal.open(
-            uri,
-            () => {
-              console.log("QR Code Modal closed");
-            },
-            true // isNode = true
-          );
-        });
-      }
-
-      // Subscribe to connection events
-      walletConnector.on("connect", async (error, payload) => {
-        if (error) {
-          throw error;
-        }
-      
-        // Close QR Code Modal
-        WalletConnectQRCodeModal.close(
-          true // isNode = true
-        );
-        
-        // Get provided accounts and chainId
-        const { accounts, chainId } = payload.params[0];
-
-        // Get Nfts
-        const { data } = await Axios.get('https://deep-index.moralis.io/api/v2/' + accounts[0] + '/nft/' + process.env.REACT_APP_TOKEN_ADDRESS + '?chain=' + convertIdToStr(chainId) + '&format=decimal', {headers : {"X-API-KEY": process.env.REACT_APP_MORALIS_KEY}});
-        console.log(data);
-
-        // Check if the Nft is owned by the user
-        if (data && data.result && data.result.length !== 0)
-          setBgClr("blue");
-        else
-          setBgClr("red");
-      });
-
-      walletConnector.on("session_update", async (error, payload) => {
-        if (error) {
-          throw error;
-        }
-      
-        // Get updated accounts and chainId by the user
-        const { accounts, chainId } = payload.params[0];
-
-        // Get Nfts
-        const { data } = await Axios.get('https://deep-index.moralis.io/api/v2/' + accounts[0] + '/nft/' + process.env.REACT_APP_TOKEN_ADDRESS + '?chain=' + convertIdToStr(chainId) + '&format=decimal', {headers : {"X-API-KEY": process.env.REACT_APP_MORALIS_KEY}});
-        console.log(data);
-
-        // Check if the Nft is owned by the user
-        if (data && data.result && data.result.length !== 0)
-          setBgClr("blue");
-        else
-          setBgClr("red");
-      });
-
-      walletConnector.on("disconnect", (error, payload) => {
-        if (error) {
-          throw error;
-        }
-      
-        // Delete walletConnector
-      });
+        return false
     }
-    fetchData();
-  });
-  return (
-    <div
-      style={{
-        backgroundColor: bgClr,
-        width: '100px',
-        height: '100px'
-      }}
-    />
-  );
+
+    const checkSpecifiedToken = async (address, chainId, tokenAddress) => {
+        const contract = new web3.eth.Contract(tokenABI, tokenAddress);
+        contract.methods.balanceOf(address).call((error, balance) => {
+            if (error)
+                throw error
+            if (balance > 0) {
+                setHaveToken(true)
+            } else {
+                setHaveToken(false)
+            }
+        });
+    }
+
+    useEffect(() => {
+        if (!web3 || !connected)
+            return
+
+        const accountsChangedCallback = async (accounts) => {
+            const chainId = await web3.eth.getChainId();
+            await verifyUser(accounts[0])
+            await checkSpecifiedToken(accounts[0], chainId, tokenAddress)
+        }
+
+        const chainChangedCallback = async (chainId) => {
+            window.location.reload();
+        }
+
+        const disconnectCallback = (data) => {
+            setConnected(false)
+        }
+
+        web3.givenProvider.on('accountsChanged', accountsChangedCallback);
+        web3.givenProvider.on('chainChanged', chainChangedCallback);
+        web3.givenProvider.on("disconnect", disconnectCallback);
+
+        (async () => {
+            const address = (await web3.eth.getAccounts())[0];
+            const chainId = await web3.eth.getChainId();
+            await verifyUser(address)
+            await checkSpecifiedToken(address, chainId, tokenAddress)
+
+            web3.eth.subscribe('logs', {
+                address: tokenAddress
+            }, function (error, result) {
+            }).on("data", function (log) {
+                web3.eth.getTransaction(log.transactionHash)
+                    .then(function (transaction) {
+                        if (transaction.from === address || transaction.to === address) {
+                            checkSpecifiedToken(address, chainId, tokenAddress)
+                        }
+                    })
+            })
+        })()
+
+        return () => {
+            web3.givenProvider.removeListener('accountsChanged', accountsChangedCallback);
+            web3.givenProvider.removeListener('chainChanged', chainChangedCallback);
+            web3.givenProvider.removeListener("disconnect", disconnectCallback);
+            web3.eth.clearSubscriptions();
+        }
+    }, [web3, connected, verifyUser, checkSpecifiedToken, tokenAddress]);
+
+    const initWalletConnect = async () => {
+        //  Create WalletConnect Provider
+        const provider = new WalletConnectProvider({
+            infuraId: process.env.REACT_APP_INFURA_ID,
+        });
+
+        //  Enable session (triggers QR Code modal)
+        await provider.enable();
+        setConnected(true)
+        setWeb3(new Web3(provider));
+    }
+
+    const initMetamask = async () => {
+        if (typeof window.ethereum === 'undefined') {
+            console.log('MetaMask is not installed!');
+            return
+        }
+
+        await window.ethereum.request({method: 'eth_requestAccounts'});
+        setConnected(true)
+        setWeb3(new Web3(window.ethereum));
+    }
+
+    return (
+        <div>
+            <p>Connected: {connected.toString()}; Verified: {verified.toString()}; Have NFT: {haveToken.toString()}</p>
+            <hr/>
+            <p>Have access to JWT? {(connected && verified && haveToken).toString()}</p>
+            <hr/>
+            <button onClick={initWalletConnect}>Connect to WalletConnect</button>
+            <button onClick={initMetamask}>Connect to Metamask</button>
+            <hr/>
+            <p>Please check the web console for messages</p>
+        </div>
+    );
 }
 
 export default App;
